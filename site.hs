63ad9c415cba170afa90260e4a2e1e6fdebab8a4
vhsconnect <90valentin@gmail.com>
2024-07-25 23:41:16 +0200

    linting

--- site.hs
+++ site.hs
@@ -1,7 +1,6 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Monoid ( mappend )
 import Hakyll
 
 --------------------------------------------------------------------------------
@@ -13,21 +12,22 @@ main = hakyllWith config $ do
     match "css/*" $ do
         route idRoute
         compile compressCssCompiler
-    match (fromList [ "about.rst", "contact.markdown" ]) $ do
+    match (fromList ["about.rst", "contact.markdown"]) $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/default.html" defaultContext
-            >>= relativizeUrls
-    -- build tags
+        compile $
+            pandocCompiler
+                >>= loadAndApplyTemplate "templates/default.html" defaultContext
+                >>= relativizeUrls
     tags <- buildTags "posts/*" (fromCapture "tags/*.html")
     tagsRules tags $ \tag pattern -> do
         let title = "Posts tagged \"" ++ tag ++ "\""
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll pattern
-            let ctx = constField "title" title
-                      `mappend` listField "posts" (postCtxWithTags tags) (return posts)
-                      `mappend` defaultContext
+            let ctx =
+                    constField "title" title
+                        `mappend` listField "posts" (postCtxWithTags tags) (return posts)
+                        `mappend` defaultContext
 
             makeItem ""
                 >>= loadAndApplyTemplate "templates/tag.html" ctx
@@ -36,19 +36,21 @@ main = hakyllWith config $ do
 
     match "posts/*" $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html" (postCtxWithTags tags)
-            >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
-            >>= relativizeUrls
-    
+        compile $
+            pandocCompiler
+                >>= loadAndApplyTemplate "templates/post.html" (postCtxWithTags tags)
+                >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
+                >>= relativizeUrls
+
     -- tags
-    create [ "archive.html" ] $ do
+    create ["archive.html"] $ do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx = listField "posts" postCtx (return posts)
-                    `mappend` constField "title" "Archives"
-                    `mappend` defaultContext
+            let archiveCtx =
+                    listField "posts" postCtx (return posts)
+                        `mappend` constField "title" "Archives"
+                        `mappend` defaultContext
             makeItem ""
                 >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
@@ -57,25 +59,26 @@ main = hakyllWith config $ do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx = listField "posts" postCtx (return posts)
-                    `mappend` defaultContext
-            getResourceBody >>= applyAsTemplate indexCtx
+            let indexCtx =
+                    listField "posts" postCtx (return posts)
+                        `mappend` defaultContext
+            getResourceBody
+                >>= applyAsTemplate indexCtx
                 >>= loadAndApplyTemplate "templates/default.html" indexCtx
                 >>= relativizeUrls
     match "templates/*" $ compile templateBodyCompiler
-
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
 
--- tags >
---(field "tags" $ return . ) `mappend` dateField "date" "%B %e, %Y" `mappend`
--- defaultContext
---
 postCtxWithTags :: Tags -> Context String
 postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
 
 config :: Configuration
-config = defaultConfiguration { previewPort
-    = 8700, destinationDirectory = "./docs" }
+config =
+    defaultConfiguration
+        { previewPort = 8700
+        , providerDirectory = "./site"
+        , destinationDirectory = "./docs"
+        }
